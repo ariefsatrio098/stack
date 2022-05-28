26e731138b4a835867f5c7780c9f3939d798078c
ariefsatrio098 <ariefsatrio098@gmail.com>
2022-05-28 09:47:33 +0700

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+char normal[10], rev[10];
+int n_top = -1, r_top = -1;
+
+void n_push(char val) {
+	n_top++;
+	normal[n_top] = val;
+}
+
+void n_pop() {
+	n_top--;
+}
+
+void r_push(char val) {
+	r_top++;
+	rev[r_top] = val;
+}
+
+void r_pop() {
+	r_top--;
+}
+
+void parse_to_stack(string s) {
+	for (int i = 0; i < (int)s.length(); i++) {
+		n_push(s[i]);
+	}
+}
+
+void reverse_from_stack() {
+	for (int i = n_top; i > -1 ; i--) {
+		r_push(normal[i]);
+	}
+}
+
+int main() {
+	string target = "gajahajag";
+
+	parse_to_stack(target);
+
+	reverse_from_stack();
+
+	if (strcmp(normal, rev) == 0) {
+		cout << target << " adalah palindrome" << endl;
+	}
+	else {
+		cout << target << " bukan palindrome" << endl;
+	}
+
+}
